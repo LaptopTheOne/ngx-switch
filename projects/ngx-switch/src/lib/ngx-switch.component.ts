da6e66157b5d307347a468ed3a74b246bccba0af
LaptopTheOne <moneywithin@yahoo.com>
2019-11-25 19:54:25 +0100

    Added url for css.

--- projects/ngx-switch/src/lib/ngx-switch.component.ts
+++ projects/ngx-switch/src/lib/ngx-switch.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-ngx-switch',
   templateUrl: './ngx-switch.component.html',
-  styles: []
+  styleUrls: ['./ngx-switch.component.css']
 })
 export class NgxSwitchComponent implements OnInit {
 
