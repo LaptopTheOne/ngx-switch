dbb26e8b2c4c8f70d74fcdc6034a6f6022ec2f52
LaptopTheOne <moneywithin@yahoo.com>
2019-11-25 19:50:27 +0100

    Added initial html and css template and style files.

--- projects/ngx-switch/src/lib/ngx-switch.component.ts
+++ projects/ngx-switch/src/lib/ngx-switch.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-ngx-switch',
-  template: `
-    <p>
-      ngx-switch works!
-    </p>
-  `,
+  templateUrl: './ngx-switch.component.html',
   styles: []
 })
 export class NgxSwitchComponent implements OnInit {
